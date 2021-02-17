aaab4be7bb32b890a6037d6896e1cdbc8d02909f
scfitze <scfitze@gmail.com>
2021-02-16 20:30:43 -0700

    adding place marker

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 const int N = 3; //size of array
 
-//0 is O in tic tac toe
-//1 is X, and 2 means the board is empty
+//1 is O in tic tac toe
+//2 is X, and 0 means the board is empty
 
 
 void DisplayBoard(int** arr){
@@ -23,23 +23,29 @@ int** CreateBoard(){
     for(int i =0; i < N; ++i){
         arr[i] = new int[N];
         for(int j = 0; j<N; ++j){
-            arr[i][j] = 2;
+            arr[i][j] = 0;
         }
     }
     return arr;
 }
 
-int GetPlayerChoice(int **arr){
+int* GetPlayerChoice(int **arr){
     cout << "Where on the board would you like to go to?" << endl;
-    cout << "First choose the row";
+    cout << "First choose the row (0,1,2): ";
     string i;
     cin >> i;
     int i1 = stoi(i);
-    cout << "Now choose the colum";
+    cout << "Now choose the colum (0,1,2): ";
     string j;
     cin >> j;
     int j1 = stoi(j);
-    return arr[i1][j1];
+    int* location;
+    location = &(arr[i1][j1]);
+    return location;
+}
+
+void PlaceMarker(int* location, const int marker) {
+    *(location) = marker;
 }
 
 int main(){
@@ -48,13 +54,23 @@ int main(){
     DisplayBoard(arr);
     cout << "Let's Play Tic-Tac-Toe!" << endl;
     int turns = 0;
-    bool whoTurn = true; //true if player 1, false for player 2
-    while(turns <= 9){
-        //whoTurn = true;
-        GetPlayerChoice(arr);
-        //whoTurn = false; 
-        turns++;
+    int* loc;
+    /*DisplayBoard(arr);
+    loc = GetPlayerChoice(arr);
+    *(loc) = 1;
+    DisplayBoard(arr);*/
+
+    while(turns < 9){
+        //first player 
+        loc = GetPlayerChoice(arr);
+        PlaceMarker(loc, 1);
         DisplayBoard(arr);
+        turns++;
+        //second player
+        loc = GetPlayerChoice(arr);
+        PlaceMarker(loc, 2);
+        DisplayBoard(arr);
+        turns++;
     }
     return 0;
 }
